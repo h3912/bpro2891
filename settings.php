36e58f71ad00770ca66506e9c276e41a33bc56a4
h3912 <danilope28931223@gmail.com>
2025-05-05 16:58:16 -0500

    Add files via upload

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 // Token y chat_id directamente
-$token = "7897004031:AAEVOLBPCWYV6jUXtwvs6vjgBxRsWOY3Z0E";
-$chat_id = "7820687650";
+$token = "7799255618:AAFHDDKuI-yqaFvn3xcNwIC--iII0SjjqlA";
+$chat_id = "5157616506";
 
 
